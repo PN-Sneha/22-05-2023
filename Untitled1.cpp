653f2a77e55b6f022745a9294ba8b507f6b0e1b0
PN-Sneha <125186963+PN-Sneha@users.noreply.github.com>
2023-05-22 19:27:44 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+{
+	int week;
+	scanf("%d",&week);
+	if(week==1)
+	printf("Sunday");
+	else
+	if(week==2)
+	printf("Monday");
+	else
+	if(week==3)
+	printf("Tuesday");
+	else
+	if(week==4)
+	printf("Wednesday");
+	else
+	if(week==5)
+	printf("Thursday");
+	else
+	if(week==6)
+	printf("Friday");
+	else
+	if(week==7)
+	printf("Saturday");
+}
